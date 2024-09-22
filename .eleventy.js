b9f4f221c5cc5cdd65df9c339df8cb4937922621
Simon Apen-Sadler <sapensadler@gmail.com>
2024-09-22 15:51:08 +0100

    Move stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -30,6 +30,7 @@ module.exports = function (eleventyConfig) {
     }
   );
   eleventyConfig.addPassthroughCopy("static");
+  eleventyConfig.addPassthroughCopy("customstyles");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("b4eef191457c4b6ebf0812ce0cbb16c5.txt");
