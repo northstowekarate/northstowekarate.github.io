4636774a45006939855e9115172f2c42cf9b90cf
Simon Apen-Sadler <sapensadler@gmail.com>
2025-02-16 11:32:04 +0000

    Update styles, bootstrap map files

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
 // Only one module.exports per configuration file, please!
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("static");
-  eleventyConfig.addPassthroughCopy("customstyles");
   eleventyConfig.addPassthroughCopy("robots.txt");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("b4eef191457c4b6ebf0812ce0cbb16c5.txt");
